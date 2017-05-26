e85bb25bd9462a4655c83184987d04270cfb5b69
Rodrigo Siqueira <rodrigo.siqueira.dev@gmail.com>
2017-05-26 00:24:42 -0300

    Tela inicial, lista de opções

--- app/src/main/java/br/eti/rodrigosiqueira/bigrock/BigRockListActivity.java
+++ app/src/main/java/br/eti/rodrigosiqueira/bigrock/BigRockListActivity.java
@@ -2,12 +2,24 @@ package br.eti.rodrigosiqueira.bigrock;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class BigRockListActivity extends AppCompatActivity {
+
+    private final String[] options = new String[]{
+      "TAREFAS", "RESUMO", "SINCRONIZAÇÃO"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_big_rock_list);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, options));
+        listView.setTextFilterEnabled(true);
     }
 }
