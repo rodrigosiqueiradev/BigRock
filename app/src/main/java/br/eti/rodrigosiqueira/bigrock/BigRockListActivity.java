401bdb57b72933fa99f96a4edf4abbe36c258453
Rodrigo Siqueira <rodrigo.siqueira.dev@gmail.com>
2017-05-26 00:32:20 -0300

    Tela inicial, lista de opções

--- app/src/main/java/br/eti/rodrigosiqueira/bigrock/BigRockListActivity.java
+++ app/src/main/java/br/eti/rodrigosiqueira/bigrock/BigRockListActivity.java
@@ -2,8 +2,11 @@ package br.eti.rodrigosiqueira.bigrock;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -20,6 +23,11 @@ public class BigRockListActivity extends AppCompatActivity {
 
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, options));
-        listView.setTextFilterEnabled(true);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), Long.toString(id), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
