5200405397bd1ec8fa54c5b92acfc96e2d839d02
Leonam Raone <leonam.raone@hotmail.com>
2017-06-04 12:37:10 -0300

    Login screen start

---
+++ app/src/main/java/br/eti/rodrigosiqueira/bigrock/LoginActivity.java
@@ -0,0 +1,31 @@
+package br.eti.rodrigosiqueira.bigrock;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnLogin = (Button) findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), BigRockActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+}
