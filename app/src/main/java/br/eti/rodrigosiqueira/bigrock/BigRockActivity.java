46321105690430fff017c2560ea152c45d1d5636
luiz.massa <luimassapontes@gmail.com>
2017-06-05 20:38:23 -0300

    Refatoracao da tela inicial

--- app/src/main/java/br/eti/rodrigosiqueira/bigrock/BigRockActivity.java
+++ app/src/main/java/br/eti/rodrigosiqueira/bigrock/BigRockActivity.java
@@ -45,26 +45,6 @@ public class BigRockActivity extends AppCompatActivity {
             }
         });
 
-        ListView listView = (ListView) findViewById(R.id.listView);
-        listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, options));
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent;
-                switch (position) {
-                    case 0:
-                        intent = new Intent(getApplicationContext(), BigRockGridActivity.class);
-                        startActivity(intent);
-                        break;
-                    case 1:
-                        intent = new Intent(getApplicationContext(), SliderTarefas.class);
-                        startActivity(intent);
-                        break;
-                    case 2:
-                        break;
-                }
-            }
-        });
     }
 
     protected void openWindows(int position) {
