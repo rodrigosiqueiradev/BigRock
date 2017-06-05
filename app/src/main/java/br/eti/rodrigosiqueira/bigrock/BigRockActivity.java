6168111eaf0d1450fd26d7d254eb74beabf8c353
luiz.massa <luimassapontes@gmail.com>
2017-06-05 20:37:39 -0300

    Refatoracao da tela inicial

--- app/src/main/java/br/eti/rodrigosiqueira/bigrock/BigRockActivity.java
+++ app/src/main/java/br/eti/rodrigosiqueira/bigrock/BigRockActivity.java
@@ -3,10 +3,12 @@ package br.eti.rodrigosiqueira.bigrock;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 public class BigRockActivity extends AppCompatActivity {
 
@@ -19,6 +21,29 @@ public class BigRockActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_big_rock);
+
+        CardView cardView1 = (CardView) findViewById(R.id.cv);
+        CardView cardView2 = (CardView) findViewById(R.id.cv1);
+
+        cardView1.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+                intent = new Intent(getApplicationContext(), BigRockGridActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        cardView2.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+                intent = new Intent(getApplicationContext(), SliderTarefas.class);
+                startActivity(intent);
+            }
+        });
 
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, options));
@@ -40,5 +65,21 @@ public class BigRockActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    protected void openWindows(int position) {
+        Intent intent;
+        switch (position) {
+            case 0:
+                intent = new Intent(getApplicationContext(), BigRockGridActivity.class);
+                startActivity(intent);
+                break;
+            case 1:
+                intent = new Intent(getApplicationContext(), SliderTarefas.class);
+                startActivity(intent);
+                break;
+            case 2:
+                break;
+        }
     }
 }
