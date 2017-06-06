97f768af8d0036b5b0bea409c8f26046f277208b
Rodrigo Siqueira <rodrigo.siqueira.dev@gmail.com>
2017-06-06 00:22:47 -0300

    Sample posição GPS

--- app/src/main/java/br/eti/rodrigosiqueira/bigrock/CardTarefa.java
+++ app/src/main/java/br/eti/rodrigosiqueira/bigrock/CardTarefa.java
@@ -1,8 +1,18 @@
 package br.eti.rodrigosiqueira.bigrock;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +29,9 @@ public class CardTarefa extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
+    private LocationListener listener;
+    private LocationManager locationManager;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_card_tarefa, container, false);
@@ -29,34 +42,71 @@ public class CardTarefa extends Fragment implements OnMapReadyCallback {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        //PLACEHOLDER FOR TASK CONTENT
         TextView status = (TextView) view.findViewById(R.id.card_status);
         status.setText("Status Maroto");
 
         TextView nome = (TextView) view.findViewById(R.id.card_name);
         nome.setText("Nome do mal");
 
-
         TextView desc = (TextView) view.findViewById(R.id.card_description);
         desc.setText("Ta molhado lá fora");
+        //PLACEHOLDER FOR TASK CONTENT
 
+        //LOAD MAP WITH FAKE LOCATION
         MapView mapView = (MapView) view.findViewById(R.id.card_map);
         mapView.onCreate(savedInstanceState);
         mapView.onResume();
-
         mapView.getMapAsync(this);
+
+        locationManager = (LocationManager) view.getContext().getSystemService(Context.LOCATION_SERVICE);
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.d("CardTarefa","Location: " + location.getLatitude() + ", " + location.getLongitude());
+                mMap.clear();
+                mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(),location.getLongitude())).title("To Aqui"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(),location.getLongitude())));
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        if(ActivityCompat.checkSelfPermission(view.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(view.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.INTERNET
+                }, 10);
+            }
+            return;
+        }
+
+        locationManager.requestLocationUpdates("gps",1000,0,listener);
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.getUiSettings().setAllGesturesEnabled(false);
-        mMap.setMinZoomPreference(16.0f);
-        LatLng latLng = new LatLng(-8.186702, -34.918896);
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(latLng);
-        markerOptions.title("Tarefa aqui zé ruela");
-        mMap.addMarker(markerOptions);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
     }
 
     public CardTarefa() {
