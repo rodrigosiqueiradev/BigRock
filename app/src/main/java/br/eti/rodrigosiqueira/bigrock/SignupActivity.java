079b27d58562e947778110670a5d403049c14859
Leonam Raone <leonam.raone@hotmail.com>
2017-06-18 13:38:22 -0300

    Validation of signup screen fields

--- app/src/main/java/br/eti/rodrigosiqueira/bigrock/SignupActivity.java
+++ app/src/main/java/br/eti/rodrigosiqueira/bigrock/SignupActivity.java
@@ -4,11 +4,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import br.eti.rodrigosiqueira.bigrock.model.User;
 
 public class SignupActivity extends AppCompatActivity {
 
     private TextView tvLogin;
+    private Button btnSignup;
+    private EditText edtName;
+    private EditText edtEmail;
+    private EditText edtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +27,17 @@ public class SignupActivity extends AppCompatActivity {
         overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
 
         tvLogin = (TextView) findViewById(R.id.link_login);
+        btnSignup = (Button) findViewById(R.id.btn_signup);
+        edtName = (EditText) findViewById(R.id.edt_name);
+        edtEmail = (EditText) findViewById(R.id.edt_email);
+        edtPassword = (EditText) findViewById(R.id.edt_password);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signup();
+            }
+        });
 
         tvLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +48,48 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void signup() {
+
+        User user = new User();
+        user.setEmail(edtEmail.getText().toString());
+        user.setName(edtName.getText().toString());
+        user.setPassword(edtPassword.getText().toString());
+
+        if (!validate(user)) {
+            Toast.makeText(getBaseContext(), "Não foi possível realizar o cadastro!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+    }
+
+    public boolean validate(User user) {
+
+        boolean valid = true;
+
+        if (user.getName().isEmpty() || user.getName().length() < 3) {
+            edtName.setError("O nome deve conter no mínimo 3 caracteres!");
+            valid = false;
+        } else {
+            edtName.setError(null);
+        }
+
+        if (user.getEmail().isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(user.getEmail()).matches()) {
+            edtEmail.setError("Informe um endereço email válido!");
+            valid = false;
+        } else {
+            edtEmail.setError(null);
+        }
+
+        if (user.getPassword().isEmpty() || user.getPassword().length() < 4 || user.getPassword().length() > 20) {
+            edtPassword.setError("A senha deve conter entre 4 e 20 caracteres!");
+            valid = false;
+        } else {
+            edtPassword.setError(null);
+        }
+
+        return valid;
     }
 
     @Override
